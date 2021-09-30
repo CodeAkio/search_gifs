a519d1b2d22bfc236ad42176d95b31d47d024338
CodeAkio <siqueira_2004@hotmail.com>
2021-09-30 11:45:55 -0300

    Search gifs

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -8,6 +12,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null) {
+      var url =
+          "https://api.giphy.com/v1/gifs/trending?api_key=9OtDbuZCWsE64PsR99ymLwVs1WPFpYt5&limit=20&rating=g";
+      var uri = Uri.parse(url);
+      response = await http.get(uri);
+    } else {
+      var url =
+          "https://api.giphy.com/v1/gifs/search?api_key=9OtDbuZCWsE64PsR99ymLwVs1WPFpYt5&q=$_search&limit=20&offset=$_offset&rating=g";
+      var uri = Uri.parse(url);
+      response = await http.get(uri);
+    }
+
+    return json.decode(response!.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
